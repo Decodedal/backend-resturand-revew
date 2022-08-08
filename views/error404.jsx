8d711d0fbde62106711350700b61284f9d932494
Decodedal <Palumbodallas@gmail.com>
2022-08-08 19:45:01 -0400

    added pictures

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,7 +6,11 @@ function error404(){
         <Def>
             <main>
                 <h1>Error 404: PAGE NOT FOUND</h1>
-                <p>Oops, sorry, we can't find this page!</p>
+                <img src='./images/cat.jpg' alt='kitty'></img>
+                <div className = 'center-me'>
+                Photo by <a href='https://unsplash.com/photos/75715CVEJhI?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink'>Chad Montano</a> on <a href='https://unsplash.com/'>Unsplash</a>
+            </div>
+                <p className='center-me'><b>Oops, sorry, we can't find this page!</b></p>
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ return(
     <Def>
         <main>
             <h1>HOME</h1>
+            <div>
+                <img src="./images/pancakes.jpg" alt='pancakes'></img>
+            </div>
+            <div className = 'center-me'>
+                Photo by <a href='https://unsplash.com/photos/eeqbbemH9-c?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink'>Chad Montano</a> on <a href='https://unsplash.com/'>Unsplash</a>
+            </div>
             <button className='btn-primary'>Places Page</button>
         </main>
     </Def>
