77989eb5d07b33ec158b3d2d5199c89cc28603f5
Decodedal <Palumbodallas@gmail.com>
2022-08-08 19:54:31 -0400

    added css!

--- controllers/places.js
+++ controllers/places.js
@@ -6,16 +6,16 @@ router.get('/',(req,res)=>{
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/rest1.jpg'
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/rest2.jpg'
       }]
       
-    res.render('places/index', {places})
+    res.render('places/index', {places })
 })
 
 module.exports = router
