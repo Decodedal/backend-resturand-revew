319f18e3a7978763a4c6e9c880b922fac25389ab
Dallas Palumbo <palumbodallas@gmail.com>
2022-08-09 17:28:06 -0400

    jazzed up form

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ app.set('view engine','jsx')
 app.engine('jsx',require('express-react-views').createEngine())
 app.use(express.static('public'))
 
-app.use('/places', require('./controllers/places'))
+app.use('/places/', require('./controllers/places'))
 
 app.get('/',(req,res)=>{
     // res.status(404).send('Hello User')
     res.render('home')
 })
+
 
 app.get('*',(req,res)=>{
    res.render('error404')
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -4,8 +4,34 @@ const Def = require('../default');
 function new_form(){
     return(
         <Def>
+            <head>
+            <link rel='stylesheet' href='style.css'></link>
+            </head>
           <main>
             <h1>Add a new Place</h1>
+            <form method='POST' action='/places'>
+                <div className='form-group'>
+                    <label htmlFor='name'>Place Name</label>
+                    <input className="form-control" id="name" name='name' required/>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='pic'>Place Picture</label>
+                    <input className="form-control" type='url' id="pic" name = 'pic'/>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='city'>City Name</label>
+                    <input className="form-control" id='city' name='city'></input>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='state'>State Name</label>
+                    <input className="form-control" id='state' name ='state'></input>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='cuisines'>Cuisine Type</label>
+                    <input className="form-control" id='cuisines' name='cuisines' required></input>
+                </div>
+                   <input className='btn btn-primary' type="submit" value="Add Place"/>
+            </form>
           </main>
         </Def>
     )
