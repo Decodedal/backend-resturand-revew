5f97020d7be236a3d8fac1652c8ed196cd83bb83
Decodedal <Palumbodallas@gmail.com>
2022-08-11 17:07:55 -0400

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -5,18 +5,34 @@ router.get('/',(req,res)=>{
     res.render('places/index', {places })
 })
 
+router.get('/new',(req,res)=>{
+  res.render('places/new')
+})
+
 router.get('/:id',(req,res)=>{
   let id = Number(req.params.id)
   if(isNaN(id)){
     res.render('error404')
   }else{
-    res.render('places/show')
+    res.render('places/show',{place: places[id], id })
   }
-  res.render('places/show',{place: places[id]})
 })
 
-router.get('/new',(req,res)=>{
-  res.render('places/new')
+router.get('/:id/edit',(req,res)=>{
+  res.send("Edit page goes here")
+})
+
+router.delete('/:id',(req,res)=>{
+  let id = Number(req.params.id)
+  if(isNaN(id)){
+    res.render('error404')
+  }else if(!places[id]){
+    res.render('error404')
+  }
+  else{
+    places.splice(id,1)
+    res.redirect('/places')
+  }
 })
 
 router.post('/',(req,res)=>{
--- models/places.js
+++ models/places.js
@@ -9,5 +9,5 @@ module.exports = [{
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: '/images/rest2.jpg'
+        pic: 'https://images.unsplash.com/photo-1507133750040-4a8f57021571?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
       }]
